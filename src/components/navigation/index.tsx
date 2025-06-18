ea9014a08b2a0ffeb5e6b391f15b25ef77ace3d9
gokul2908 <gokulkumar2908@gmail.com>
2025-06-18 19:00:06 +0530

    ui fix

--- src/components/navigation/index.tsx
+++ src/components/navigation/index.tsx
@@ -3,41 +3,23 @@ import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import IconButton from "@mui/material/IconButton";
-import Typography from "@mui/material/Typography";
-import Menu from "@mui/material/Menu";
 import MenuIcon from "@mui/icons-material/Menu";
 import Container from "@mui/material/Container";
-import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
-import Tooltip from "@mui/material/Tooltip";
-import MenuItem from "@mui/material/MenuItem";
 import InboxIcon from "@mui/icons-material/MoveToInbox";
 import MailIcon from "@mui/icons-material/Mail";
 
-// const pages = ['Products', 'Pricing', 'Blog'];
-const settings = ["Profile", "Account", "Dashboard", "Logout"];
-
 function ResponsiveAppBar({ logo, navigation, contactNo, resumeLink }: Props) {
     const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(
-        null
-    );
-    const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(
         null
     );
 
     const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
         setAnchorElNav(event.currentTarget);
     };
-    const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
-        setAnchorElUser(event.currentTarget);
-    };
 
     const handleCloseNavMenu = () => {
         setAnchorElNav(null);
-    };
-
-    const handleCloseUserMenu = () => {
-        setAnchorElUser(null);
     };
 
     const MyLink = ({ text, index }) => {
@@ -74,7 +56,7 @@ function ResponsiveAppBar({ logo, navigation, contactNo, resumeLink }: Props) {
                 {navigation.map((text, index) => (
                     <ListItem key={text} disablePadding>
                         <ListItemButton>
-                            <MyLink text={text} index={index}/>
+                            <MyLink text={text} index={index} />
                         </ListItemButton>
                     </ListItem>
                 ))}
@@ -132,7 +114,7 @@ function ResponsiveAppBar({ logo, navigation, contactNo, resumeLink }: Props) {
                             flexGrow: 1,
                         }}
                     >
-                        <a href=".">
+                        <a href="." style={{ transform: "translateX(-50%)" }}>
                             <img
                                 src={logo}
                                 alt="Logo"
@@ -177,7 +159,6 @@ function ResponsiveAppBar({ logo, navigation, contactNo, resumeLink }: Props) {
 }
 export default ResponsiveAppBar;
 
-// import Button as customBtn from '../customButton';
 import "./style.css";
 import {
     Drawer,
@@ -193,70 +174,4 @@ interface Props {
     contactNo?: number;
     navigation: string[];
     resumeLink?: string;
-}
-
-function Nav({ logo, navigation, contactNo }: Props): any {
-    const options = [
-        "Home",
-        "Services",
-        "Testimonial",
-        "FAQ",
-        "Portfolio",
-        "Contacts",
-    ];
-    console.log(navigation);
-    return (
-        <nav className="navbar navbar-expand-lg navbar-dark menu shadow fixed-top px-3">
-            <a className="navbar-brand" href="#">
-                <img src={logo} alt="logo_image" />
-            </a>
-            <button
-                className="navbar-toggler bg-primary bg-opacity-75"
-                type="button"
-                data-bs-toggle="collapse"
-                data-bs-target="#navbarSupportedContent"
-                aria-controls="navbarSupportedContent"
-                aria-expanded="false"
-                aria-label="Toggle navigation"
-            >
-                <span className="navbar-toggler-icon"></span>
-            </button>
-            <div
-                className="collapse navbar-collapse justify-content-end"
-                id="navbarSupportedContent"
-            >
-                <ul className="navbar-nav mb-2 mb-lg-0">
-                    <NavSideOptions options={navigation} />
-                </ul>
-                {/* {!!contactNo && (
-                    <Button
-                        text={'+91' + contactNo}
-                        icon={<i className='fas fa-phone' />}
-                    />
-                )} */}
-            </div>
-        </nav>
-    );
-}
-
-function NavSideOptions({
-    active,
-    options,
-}: {
-    active?: boolean;
-    options: Array<string>;
-}) {
-    return (
-        <>
-            {options.map((option, i) => {
-                return (
-                    <li className="nav-item" key={i}>
-                        <a className="nav-link" href={"#" + option}>
-                            {option}
-                        </a>
-                    </li>
-                );
-            })}
-        </>
-    );
 }
